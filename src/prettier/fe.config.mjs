c6b8fad50906ae9dc34a15876e6600c9769fbcb3
vlad.olishkevych <olishkevich123@gmail.com>
2025-12-31 18:45:47 +0100

    feat: add Prettier configuration files for backend and frontend

---
+++ src/prettier/fe.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import("prettier").Config} */
+export default {
+  singleQuote: true,
+  trailingComma: "all",
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  importOrderParserPlugins: [
+    "classProperties",
+    "decorators-legacy",
+    "typescript",
+  ],
+  importOrder: [
+    "^react(\\/.*)?$",
+    "^next(\\/.*)?$",
+    "<THIRD_PARTY_MODULES>",
+    "^@/(.*)$",
+    "^src/(.*)$",
+    "^../(.*)",
+    "^./(.*)",
+  ],
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  printWidth: 140,
+};
